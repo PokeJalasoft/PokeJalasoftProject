548d57895655364ac829e5541a1faf8d72391b90
SebastianRosalesZ <Sebastian.Rosales@jalasoft.com>
2024-04-08 15:48:53 -0400

    DataGrid-UI population

--- src/pages/Pokedex/Pokedex.tsx
+++ src/pages/Pokedex/Pokedex.tsx
@@ -1,19 +1,24 @@
-import Layout from "./layout";
+import PokemonDataGrid from '../../components/PokemonDataGrid';
+import usePokemons from '../../hooks/usePokemons';
+import BasicCard from './layout';
 
 function Pokedex() {
+  const { pokemons, loading, error } = usePokemons();
+
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "100vh",
-        }}
-      >
-        <Layout />
-      </div>
-    </>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <BasicCard>
+        <div style={{ marginLeft: '20px' }}>
+          {loading ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <p>Error: {error.message}</p>
+          ) : (
+            <PokemonDataGrid rows={pokemons} />
+          )}
+        </div>
+      </BasicCard>
+    </div>
   );
 }
 
--- src/pages/Pokedex/layout.tsx
+++ src/pages/Pokedex/layout.tsx
@@ -1,14 +1,20 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { ReactNode } from "react";
 
-export default function BasicCard() {
+interface BasicCardProps {
+  children: ReactNode;
+}
+
+export default function BasicCard({ children }: BasicCardProps) {
   return (
     <Card variant="outlined" sx={{ minWidth: 500, minHeight: 800, backgroundColor: 'rgb(244, 245, 248)' }}>
       <CardContent>
         <Typography variant="h5" component="div">
           Pokemon Web App
         </Typography>
+        {children}
       </CardContent>
     </Card>
   );
