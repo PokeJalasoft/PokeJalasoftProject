e4a3ad9633755ff8e649fa911e03a1492a50e6c0
SebastianRosalesZ <Sebastian.Rosales@jalasoft.com>
2024-04-15 14:54:05 -0400

    Location Area Encounters Information Added

--- src/modals/DetailsModal.tsx
+++ src/modals/DetailsModal.tsx
@@ -2,7 +2,7 @@ import Modal from 'react-modal';
 import { Card, CardContent } from '@mui/material';
 import usePokemon from '@hooks/usePokemon';
 
-const customStyles = {
+const customStyles: ReactModal.Styles = {
     overlay: {
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
@@ -13,12 +13,14 @@ const customStyles = {
         bottom: 'auto',
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
-        width: 'auto',
+        width: '500px',
+        height: '400px',
         padding: 0,
         borderRadius: '10px',
         backgroundColor: 'rgb(244, 245, 248)',
         border: 'none',
         boxShadow: '0px 3px 8px rgba(0, 0, 0, 0.2)',
+        overflowY: 'auto', // overflowY type to string
     },
 };
 
@@ -69,7 +71,7 @@ const DetailsWindow: React.FC<DetailsWindowProps> = ({ isOpen, onClose, pokemon 
                                 <img src={pokemonDetails.picture} alt={pokemonDetails.name} style={{ width: '180px', height: '180px', marginRight: '5px', border: '2px solid black' }} />
                                 <div>
                                     <div><b>Name: </b>{pokemonDetails.name}</div>
-                                    <div><b>Type: </b> {pokemonDetails.types.join(', ')}</div>
+                                    <div><b>Type: </b>{pokemonDetails.types.join(', ')}</div>
                                 </div>
                             </div>
                             <div><b>Features:</b></div>
@@ -78,7 +80,18 @@ const DetailsWindow: React.FC<DetailsWindowProps> = ({ isOpen, onClose, pokemon 
                                 <li>Weight: {pokemonDetails.details.weight}</li>
                                 <li>Base Experience: {pokemonDetails.details.baseExperience}</li>
                             </ul>
-
+                            <div><b>Locations:</b></div>
+                            {pokemonDetails.locations.length > 0 ? (
+                                <ul>
+                                    {pokemonDetails.locations.map((location) => (
+                                        <li key={location}>
+                                            {location}
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <div>No known locations</div>
+                            )}
                         </CardContent>
                     </Card>
                 )}
